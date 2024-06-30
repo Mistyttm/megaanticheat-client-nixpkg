69b8d5245380b819655d11f9b86ae71457be730f
Mistyttm <contact@mistyttm.dev>
2024-06-30 17:49:41 +1000

    Got client_backend working

---
+++ default.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, fetchFromGitHub, rustPlatform }:
+
+rustPlatform.buildRustPackage rec {
+  pname = "MegaAntiCheat";
+  version = "0.0.6";
+
+  src = fetchFromGitHub {
+    owner = "MegaAntiCheat";
+    repo = "client-backend";
+    rev = "v0.0.6";
+    sha256 = "09a7pibxqj7n194v57nq7qjyb13y2iczdsy750zci0rq4ql79ip9";
+  };
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "rcon-0.5.2" = "sha256-DJkINFgIsS94/ps5ahrilZkyphKjmR7a0xYDK2mFg0Y=";
+      "steam-language-gen-0.1.2" = "sha256-KcZdf0sDJMbPDxPCZkq85eQ+MmbHnr2LKc9B1qmLuz0=";
+      "tf-demo-parser-0.5.1" = "sha256-QEUd2yTIshS2H+XO8p1ggh22tox3jgPoYybrv0MhKL8=";
+    };
+  };
+  buildInputs = [ pkgs.openssl ];
+  nativeBuildInputs = [ pkgs.pkg-config ];
+}
